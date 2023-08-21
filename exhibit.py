2e5b181766293334e97eb50b414e554ea5150abf
primatepngs <142123212+primatepngs@users.noreply.github.com>
2023-08-21 01:22:13 -0700

    Add menu bar with delay settings. Use Ape type in it's label and constrain tokenId range appropriately.
    Signed-off-by: primatepngs <142123212+primatepngs@users.noreply.github.com>

--- exhibit.py
+++ exhibit.py
@@ -10,7 +10,7 @@ from time import sleep
 from threading import Thread
 
 # Configuration options
-SLIDESHOW_DELAY = 8  # Delay between image transitions in seconds
+SLIDESHOW_DELAY = 8  # Default delay between image transitions in seconds
 CLEANUP_THRESHOLD = 50  # Number of images to keep before cleanup
 APE_RES=947
 WIDTH=APE_RES
@@ -38,7 +38,7 @@ def fetch_and_save_image(indices):
         image_CID = ipfs_url.strip('ipfs://')
         image_response = requests.get(f"https://ipfs.io/ipfs/{image_CID}", stream=True)
         if image_response.status_code == 200:
-            image_path = os.path.join(TMP_DIR, f"{ape_type}-{tokenId}.png")
+            image_path = os.path.join(TMP_DIR, f"{ape_type}_{tokenId}.png")
             with open(image_path, "wb") as f:
                 image_response.raw.decode_content = True
                 shutil.copyfileobj(image_response.raw, f)
@@ -60,7 +60,14 @@ def cleanup():
             os.remove(old)
 
 def get_random_ape():
-    return [random.randint(0,2), random.randint(0, 30007)]
+    rando = random.randint(1,10)
+    if rando <= 6: # 60% chance of Bored Ape
+        ape_type = 0
+        rand_ape_index = random.randint(0, 9999)
+    else: # 40% chance of Mutant Ape
+        ape_type = 1
+        rand_ape_index = random.randint(0, 30007)
+    return [ape_type, rand_ape_index]
 
 # Produce the show continuously, geenerating random indices, fetching and rolling cache directory forward.
 def slideshow(canvas, label):
@@ -73,12 +80,23 @@ def slideshow(canvas, label):
         if image_files:
             latest_image_file = max(image_files, key=os.path.getctime)
 
+        # Parse the filename for apetype before underscore
+        ape_type_index = os.path.basename(latest_image_file).split("_")[0]
+        match int(ape_type_index):
+            case 0:
+                ape_type = "Bored"
+            case 1:
+                ape_type = "Mutant"
+
+        if fetchedId > 29999:
+            ape_type = "Mega Mutant"
+
         # Prepare canvas
         img = Image.open(latest_image_file)
         img = img.resize((APE_RES, APE_RES), Image.LANCZOS)
         img = ImageTk.PhotoImage(img)
         canvas.create_image(WIDTH/2, HEIGHT/2, anchor="center", image=img)
-        canvas.create_text(15,20,text=f"Ape #{fetchedId}", font="Helvetica 26",anchor=tkint.NW)
+        canvas.create_text(15,20,text=f"{ape_type} Ape #{fetchedId}", font="Helvetica 26",anchor=tkint.NW)
 
         canvas.update()
 
@@ -88,9 +106,29 @@ def slideshow(canvas, label):
         sleep(SLIDESHOW_DELAY)
 
 def main():
-    # Create the main window for the slideshow
     root = tkint.Tk()
     root.title("Ape Exhibit")
+    root.option_add('*tearoff', False)
+
+    # Menu bar with Settings top-level
+    menubar = tkint.Menu(root)
+    settings_menu = tkint.Menu(menubar, tearoff=False)
+    menubar.add_cascade(label="Settings", menu=settings_menu)
+
+    root.config(menu=menubar)
+
+    # Options Menu
+    options_menu = tkint.Menu(settings_menu, tearoff=False)
+    settings_menu.add_cascade(label="Set Delay", menu=options_menu)
+
+    delay_options = [5, 8, 10, 15]
+    delay_current = tkint.IntVar()
+    delay_current.set(SLIDESHOW_DELAY) # Default from global
+
+    # Create command for each delay option value
+    for d in delay_options:
+        options_menu.add_command(label=str(d), command=delay_chosen(d))
+
     canvas = tkint.Canvas(
         root,
         height=HEIGHT,
@@ -114,6 +152,14 @@ def main():
 
     # Run the main event loop
     root.mainloop()
+
+def delay_chosen(delay):
+    return lambda: set_delay(delay)
+
+def set_delay(delay):
+    globals().update({'SLIDESHOW_DELAY': delay})
+    print("Delay changed %d", delay)
+
 
 if __name__ == '__main__':
     main()
